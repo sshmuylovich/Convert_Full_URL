f01297a2e5fdfa61e881f8bb53b4816db8774007
Amy Jiang <amyjiang@Amys-MBP.fios-router.home>
2020-10-01 22:59:16 -0400

    Katie's https suggestion

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ def main():
             print ("Error in input name")
             return
         elif len(my_list) == 1:
-            full_name = os.path.join("http://",  "www." + my_list[0] + ".edu")
+            full_name = os.path.join("https://", "www." + my_list[0] + ".edu")
         elif len(my_list) == 2:
-            full_name = os.path.join("http://", "www." + my_list[0] + '.' + my_list[1])
+            full_name = os.path.join("https://", "www." + my_list[0] + '.' + my_list[1])
         else:
-            full_name = os.path.join("http://", my_list[0] + '.' + my_list[1] +'.' + my_list[2])
+            full_name = os.path.join("https://", my_list[0] + '.' + my_list[1] +'.' + my_list[2])
 
         print ("The full name of " + sys.argv[2] + " is " + full_name)
     else:
