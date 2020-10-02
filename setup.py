e2285e507b6d8ee4a89d787ba06dea5087ed651c
Sima Shmuylovich <sshmuylovich21@andover.edu>
2020-10-01 23:44:03 -0400

    changed to the correct username

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-ajiang36", # Replace with your own username
+    name="convert-full-url-pkg-sshmuylovich", # Replace with your own username
     version="0.0.1",
-    author="Amy Jiang",
-    author_email="ajiang21@andover.edu",
+    author="Sima Shmuylovich",
+    author_email="sshmuylovich21@andover.edu",
     description="This package converts partial urls into full urls",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/sshmuylovich/Convert_Full_URL.git",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
